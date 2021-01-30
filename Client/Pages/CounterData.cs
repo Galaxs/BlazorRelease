bb1952d7d7fb78123e3ec3cd8705f3ec59654e21
pablocsar@gmail.com <pablocsar@gmail.com>
2021-01-30 00:45:31 +0000

    Child - Parent comunication between components.

---
+++ Client/Pages/CounterData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRelease.Client.Pages
+{
+    public class CounterData
+    {
+        public Action<int> CountChanged { get; set; }
+        private int count;
+        public int Count
+        {
+            get => this.count;
+            set
+            {
+                if (value != this.count)
+                {
+                    this.count = value;
+                    CountChanged?.Invoke(this.count);
+                }
+            }
+        }
+    }
+}   
---
+++ Client/Pages/Timer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace BlazorRelease.Client.Pages
+{
+    public class Timer : ComponentBase
+    {
+
+        [Parameter]
+        public double TimeInSeconds { get; set; }
+        [Parameter]
+        public EventCallback Tick { get; set; }
+        protected override void OnInitialized()
+        {
+            var timer = new System.Threading.Timer(
+            callback: (_) => InvokeAsync(() => Tick.InvokeAsync(null)), 
+            state: null,
+            dueTime: TimeSpan.FromSeconds(TimeInSeconds),
+            period: Timeout.InfiniteTimeSpan);
+        }
+    }
+}
